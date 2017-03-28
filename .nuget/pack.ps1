259210b8037e6bf7acee06d3b962b25bb1bdc9cf
Tsessebe <francois@recotrust.co.za>
2017-03-28 23:05:11 +0200

    Fixed paths to nuget in pack script

--- .nuget/pack.ps1
+++ .nuget/pack.ps1
@@ -4,9 +4,9 @@ $versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
 
 Write-Host "Setting .nuspec version tag to $versionStr"
 
-$content = (Get-Content $root\NuGet\PasswordValidation.nuspec) 
+$content = (Get-Content $root\.nuget\PasswordValidation.nuspec) 
 $content = $content -replace '\$version\$',$versionStr
 
-$content | Out-File $root\nuget\PasswordValidation.compiled.nuspec
+$content | Out-File $root\.nuget\PasswordValidation.compiled.nuspec
 
-& $root\NuGet\NuGet.exe pack $root\nuget\PasswordValidation.compiled.nuspec
+& $root\.nuget\NuGet.exe pack $root\.nuget\PasswordValidation.compiled.nuspec
