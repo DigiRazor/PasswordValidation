6b5fab5dcb71fc832612c3895806dfb184f36839
Nick Verschueren <nick.verschueren@euri.com>
2017-10-04 16:45:39 +0200

    Revert to using AssemblyInfo to support NuGet

--- DigiRazor.PasswordService/Properties/AssemblyInfo.cs
+++ DigiRazor.PasswordService/Properties/AssemblyInfo.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 using System.Reflection;
+using System.Resources;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-//[assembly: AssemblyTitle("PasswordValidation")]
-//[assembly: AssemblyDescription("")]
-//[assembly: AssemblyConfiguration("")]
-//[assembly: AssemblyCompany("DigiRazor (Pty) Ltd")]
-//[assembly: AssemblyProduct("DigiRazor.PasswordValidation")]
-//[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTitle("PasswordValidation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("DigiRazor (Pty) Ltd")]
+[assembly: AssemblyProduct("DigiRazor.PasswordValidation")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-//[assembly: NeutralResourcesLanguage("en")]
+[assembly: NeutralResourcesLanguage("en")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -33,8 +34,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-//[assembly: AssemblyVersion("1.0.0.0")]
-//[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 
 
 [assembly: CLSCompliant(true)]
